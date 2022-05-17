61ff37b4a94aba2479e50d42a389a1d210b7d406
Jason Novinger <jnovinger@gmail.com>
2022-05-17 16:36:01 -0500

    Mod for Kleomedes purposes, just straight batch payments
    These now reads in a file with the delegators and amount to refund.

--- src/slash_refund.py
+++ src/slash_refund.py
@@ -1,102 +1,121 @@
 import argparse
+import csv
 import json
-import requests
-
+import logging
+from decimal import Decimal
 from subprocess import run
 from time import sleep
 
+# import requests
 
-def getResponse(end_point, query_field=None, query_msg=None):
-    response = None
-
-    try:
-        if query_msg is not None and query_field is not None:
-            response = requests.get(end_point, params={query_field: query_msg})
-        else:
-            response = requests.get(end_point, params={})
-    except Exception as e:
-        print(e)
-
-    if response is not None and response.status_code == 200:
-        return json.loads(response.text)
-    else:
-        if response is not None:
-            print("Response Error")
-            print(str(response.status_code))
-            print(str(response.text))
-        else:
-            print("Response is None")
-
-        return None
+DENOM_EXPONENTS = {
+    'ATOM': 0,
+    'uatom': 6,
+    'OSMO': 0,
+    'uosmo': 6,
+}
+BIN_DIR = ""  # if this isn't empty, make sure it ends with a slash
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
 
-def getSlashBlock(url: str, val_address: str) -> int:
-    endpoint = url + "/block_search?query=%22slash.address=%27" + val_address + "%27%22"
-    data = getResponse(endpoint)
-    latest_slash = len(data["result"]["blocks"]) - 1
-    return data["result"]["blocks"][latest_slash]["block"]["header"]["height"]
+
+# def getResponse(end_point, query_field=None, query_msg=None):
+#     response = None
+#
+#     try:
+#         if query_msg is not None and query_field is not None:
+#             response = requests.get(end_point, params={query_field: query_msg})
+#         else:
+#             response = requests.get(end_point, params={})
+#     except Exception as e:
+#         logger.exception(e)
+#
+#     if response is not None and response.status_code == 200:
+#         return json.loads(response.text)
+#     else:
+#         if response is not None:
+#             logger.error('\n\t'.join((
+#                 "Response Error",
+#                 str(response.status_code),
+#                 str(response.text),
+#             )))
+#         else:
+#             logger.error("Response is None")
+#
+#         return None
 
 
-def getDelegationAmounts(
-    daemon: str, endpoint: str, chain_id: str, block_height: int, valoper_address: str
-):
-    endpoints = [endpoint]
-    delegations = {}
-    page = 1
-    page_limit = 200
-    more_pages = True
-
-    while more_pages:
-        endpoint_choice = (page % len(endpoints)) - 1
-        result = run(
-            f"/home/schultzie/go/bin/{daemon} q staking delegations-to {valoper_address} --height {block_height} --page {page} --output json --limit {page_limit} --node {endpoints[endpoint_choice]} --chain-id {chain_id}",
-            shell=True,
-            capture_output=True,
-            text=True,
-        )
-        if result.returncode == 1:
-            print(endpoints[endpoint_choice])
-            continue
-        response = json.loads(result.stdout)
-
-        for delegation in response["delegation_responses"]:
-            delegator_address = delegation["delegation"]["delegator_address"]
-            delegation_amount = delegation["balance"]["amount"]
-            if delegator_address not in delegations:
-                delegations[delegator_address] = delegation_amount
-            else:
-                print(delegator_address)
-        page += 1
-        sleep(2)
-        if len(response["delegation_responses"]) < page_limit < 20:
-            more_pages = False
-
-    return delegations
+# def getSlashBlock(url: str, val_address: str) -> int:
+#     endpoint = url + "/block_search?query=%22slash.address=%27" + val_address + "%27%22"
+#     data = getResponse(endpoint)
+#     latest_slash = len(data["result"]["blocks"]) - 1
+#     return data["result"]["blocks"][latest_slash]["block"]["header"]["height"]
 
 
-def calculateRefundAmounts(
-    daemon: str, endpoint: str, chain_id: str, slash_block: int, valoper_address: str
-):
-    pre_slack_block = int(slash_block) - 5
-    refund_amounts = {}
-    pre_slash_delegations = getDelegationAmounts(
-        daemon, endpoint, chain_id, pre_slack_block, valoper_address
-    )
+# def getDelegationAmounts(
+#     daemon: str, endpoint: str, chain_id: str, block_height: int, valoper_address: str
+# ):
+#     endpoints = [endpoint]
+#     delegations = {}
+#     page = 1
+#     page_limit = 200
+#     more_pages = True
 
-    post_slash_delegations = getDelegationAmounts(
-        daemon, endpoint, chain_id, slash_block, valoper_address
-    )
+#     while more_pages:
+#         endpoint_choice = (page % len(endpoints)) - 1
+#         result = run(
+#             f"{BIN_DIR}{daemon} q staking delegations-to {valoper_address} --height {block_height} --page {page} --output json --limit {page_limit} --node {endpoints[endpoint_choice]} --chain-id {chain_id}",
+#             shell=True,
+#             capture_output=True,
+#             text=True,
+#         )
+#         if result.returncode == 1:
+#             logger.info(endpoints[endpoint_choice])
+#             continue
+#         response = json.loads(result.stdout)
 
-    if len(pre_slash_delegations) != len(post_slash_delegations):
-        raise ("Something went awry on delegation calcs")
-    for delegation_address in pre_slash_delegations:
-        refund_amount = int(pre_slash_delegations[delegation_address]) - int(
-            post_slash_delegations[delegation_address]
-        )
-        if refund_amount > 100:
-            refund_amounts[delegation_address] = refund_amount
+#         for delegation in response["delegation_responses"]:
+#             delegator_address = delegation["delegation"]["delegator_address"]
+#             delegation_amount = delegation["balance"]["amount"]
+#             if delegator_address not in delegations:
+#                 delegations[delegator_address] = delegation_amount
+#             else:
+#                 logger.info(delegator_address)
+#         page += 1
+#         sleep(2)
+#         if len(response["delegation_responses"]) < page_limit < 20:
+#             more_pages = False
 
-    return refund_amounts
+#     return delegations
+
+
+# def calculateRefundAmounts(
+#     daemon: str, endpoint: str, chain_id: str, slash_block: int, valoper_address: str
+# ):
+#     pre_slack_block = int(slash_block) - 5
+#     refund_amounts = {}
+#     pre_slash_delegations = getDelegationAmounts(
+#         daemon, endpoint, chain_id, pre_slack_block, valoper_address
+#     )
+
+#     post_slash_delegations = getDelegationAmounts(
+#         daemon, endpoint, chain_id, slash_block, valoper_address
+#     )
+
+#     if len(pre_slash_delegations) != len(post_slash_delegations):
+#         raise ("Something went awry on delegation calcs")
+#     for delegation_address in pre_slash_delegations:
+#         refund_amount = int(pre_slash_delegations[delegation_address]) - int(
+#             post_slash_delegations[delegation_address]
+#         )
+#         if refund_amount > 100:
+#             refund_amounts[delegation_address] = refund_amount
+
+#     return refund_amounts
 
 
 def buildRefundJSON(
@@ -151,7 +170,7 @@ def buildRefundScript(
     batch = 0
     for batch_refund in batches:
         refundJson = buildRefundJSON(batch_refund, send_address, denom, memo)
-        with open(f"/tmp/dist_{batch}.json", "w+") as f:
+        with open(f"/tmp/dist_{denom}_{batch}.json", "w+") as f:
             f.write(json.dumps(refundJson))
         for address in batch_refund:
             batched[address] = batch_refund[address]
@@ -160,25 +179,39 @@ def buildRefundScript(
 
 
 def issue_refunds(
-    batch_count: int, daemon: str, chain_id: str, keyname: str, node: str
+    batch_count: int, daemon: str, chain_id: str, keyname: str, node: str,
+    denom: str, dry_run: bool = False,
 ):
     i = 0
     while i < batch_count:
-        result = run(
-            f"/home/schultzie/go/bin/{daemon} tx sign /tmp/dist_{i}.json --from {keyname} -ojson --output-document ~/dist_signed.json --node {node} --chain-id {chain_id} --keyring-backend test",
-            shell=True,
-            capture_output=True,
-            text=True,
+        sign_cmd = (
+            f"{BIN_DIR}{daemon} tx sign /tmp/dist_{denom}_{i}.json --from {keyname} -ojson "
+            f"--output-document ~/dist_{denom}_signed.json --node {node} --chain-id {chain_id} "
+            f"--keyring-backend test"
         )
-        sleep(1)
-        result = run(
-            f"/home/schultzie/go/bin/{daemon} tx broadcast ~/dist_signed.json --node {node} --chain-id {chain_id}",
-            shell=True,
-            capture_output=True,
-            text=True,
+        broadcast_cmd = (
+            f"{BIN_DIR}{daemon} tx broadcast ~/dist_{denom}_signed.json --node {node} "
+            f"--chain-id {chain_id}"
         )
+
         i += 1
-        sleep(15)
+        if dry_run:
+            logger.debug(f"sign cmd: {sign_cmd}")
+            logger.debug(f"broadcast cmd: {broadcast_cmd}")
+        else:
+            # sign
+            result = run(sign_cmd, shell=True, capture_output=True, text=True)
+            logger.info(f'subprocess.run() result: {result}')
+            sleep(1)
+
+            # broadcast
+            result = run(broadcast_cmd, shell=True, capture_output=True, text=True)
+            logger.info(f'subprocess.run() result: {result}')
+
+            # if this is not the last batch, sleep
+            if i < batch_count:
+                sleep(16)
+
 
 
 def parseArgs():
@@ -248,7 +281,42 @@ def parseArgs():
         required=True,
         help="Wallet to issue refunds from",
     )
+    parser.add_argument(
+        "--dry_run",
+        dest="dry_run",
+        nargs='?',
+        required=False,
+        default=False,
+        const=True,
+        help="Indicates whether this should actually broadcast transactions or not",
+    )
+    parser.add_argument(
+        "-f",
+        "--refund_file",
+        dest="refund_file",
+        required=False,
+        default=None,
+        type=open,
+        help="Indicates whether this should actually broadcast transactions or not",
+    )
     return parser.parse_args()
+
+
+def getRefundAmountsFromFile(file_obj, denom):
+    # return {
+    #     'osmos1asdkhjfaklsjfhaskjdf': 1.234567,
+    # }
+    refund_amounts = {}
+    refund_reader = csv.reader(file_obj, delimiter=',', quotechar='|')
+    denom_multiplier = 10 ** DENOM_EXPONENTS.get(denom, 1)
+    for row in refund_reader:
+        if 'address' in row[0]:
+            continue
+        delegation_addr = row[0]
+        refund_amt = Decimal(row[3]) * denom_multiplier
+        refund_amounts[delegation_addr] = refund_amt
+
+    return refund_amounts
 
 
 def main():
@@ -262,13 +330,21 @@ def main():
     send_address = args.send_address
     memo = args.memo
     keyname = args.keyname
+    refund_file = args.refund_file
+    dry_run = args.dry_run
+    logger.debug(f'DEBUG: args: {args}')
 
-    slash_block = getSlashBlock(endpoint, valcons_address)
-    refund_amounts = calculateRefundAmounts(
-        daemon, endpoint, chain_id, slash_block, valoper_address
-    )
+    # The next two calls are for calculating slash related refunds.
+    # slash_block = getSlashBlock(endpoint, valcons_address)
+    # refund_amounts = calculateRefundAmounts(
+    #     daemon, endpoint, chain_id, slash_block, valoper_address
+    # )
+
+    # This one is for other kinds of refunds (e.g. never made it to the active set)
+    refund_amounts = getRefundAmountsFromFile(refund_file, denom)
+
     batch_count = buildRefundScript(refund_amounts, send_address, denom, memo)
-    issue_refunds(batch_count, daemon, chain_id, keyname, endpoint)
+    issue_refunds(batch_count, daemon, chain_id, keyname, endpoint, denom, dry_run)
 
 
 if __name__ == "__main__":
